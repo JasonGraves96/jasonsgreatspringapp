a6050e8929361954d0f432a691c53c749c0bd74c
Jason Graves <92340845+JasonGraves96@users.noreply.github.com>
2022-03-21 15:02:57 -0400

    Add files via upload

---
+++ src/main/java/com/revature/demo/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package com.revature.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Jason's Awesome Spring Boot App!";
+    }
+
+    @GetMapping("/fruit")
+    public String fruit() {
+        return "apple banana kiwi orange lemon coconut";
+    }
+}
